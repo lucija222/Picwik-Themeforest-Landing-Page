00f68b1dc5f2def13d55c4254314e8b7a46db6a2
Lucija <lucia.hranjec@gmail.com>
2023-04-17 17:13:56 +0200

    Base TS done

--- script.ts
+++ script.ts
@@ -1,27 +1,30 @@
-// let counterElements = document.querySelectorAll(".counter");
+let counterElements: NodeListOf<HTMLElement> = document.querySelectorAll(".counter");
 
-// counterElements.forEach(element => {
-//     const startValue = 0;
-//     const endValue = parseInt(element.dataset.endValue);
-//     const duration = 2500;
-//     let startTime: number | null = null;
+counterElements.forEach((counter: HTMLElement): void => {
+    const startValue = 0;
+    const dataset: string = counter.dataset.endValue!;
+    const endValue = parseInt(dataset);
+    const duration = 2500;
+    let startTime: number | null = null;
 
-//     const numberCounterAnimation = (timestamp) => {
-//         if (!startTime) {
-//             startTime = timestamp;
-//         }
-//         const elapsedTime = timestamp - startTime;
-//         const progress = Math.min(elapsedTime / duration, 1);
-//         const value = Math.floor(progress * (endValue - startValue));
-//         element.textContent = value.toString();
-//         if (value < endValue) {
-//             requestAnimationFrame(numberCounterAnimation);
-//         }
-//     };
+    const numberCounterAnimation = (timestamp: number): void => {
+        if (!startTime) {
+            startTime = timestamp;
+        } else if (startTime) {
+            const elapsedTime = timestamp - startTime;
+            const progress = Math.min(elapsedTime / duration, 1);
+            const value = Math.floor(progress * (endValue - startValue));
+            counter.textContent = value.toString();
+            if (value < endValue) {
+                requestAnimationFrame(numberCounterAnimation);
+            }
+        }
+    };
 
-//     const start = (timestamp) => {
-//         startTime = timestamp;
-//         requestAnimationFrame(numberCounterAnimation);
-//     }
-//     requestAnimationFrame(start);
-// });
+    const start = (timestamp: number): void => {
+        startTime = timestamp;
+        requestAnimationFrame(numberCounterAnimation);
+    }
+    
+    requestAnimationFrame(start);
+});
