779d54718e98739196c140d674018af196fdea30
Lucija <lucia.hranjec@gmail.com>
2023-04-21 16:44:01 +0200

    TS for changing img on hover, finished transitions

--- script.ts
+++ script.ts
@@ -1,4 +1,6 @@
-let counterElements: NodeListOf<HTMLElement> = document.querySelectorAll(".counter");
+//--------------number counter
+let counterElements: NodeListOf<HTMLElement> =
+    document.querySelectorAll(".counter");
 
 counterElements.forEach((counter: HTMLElement): void => {
     const startValue = 0;
@@ -24,7 +26,32 @@ counterElements.forEach((counter: HTMLElement): void => {
     const start = (timestamp: number): void => {
         startTime = timestamp;
         requestAnimationFrame(numberCounterAnimation);
-    }
-    
+    };
+
     requestAnimationFrame(start);
+});
+
+//--------------------gallery anchor hover
+const imgIcons = document.querySelectorAll(".anchor img");
+
+imgIcons.forEach((imgElement) => {
+    imgElement.addEventListener("mouseenter", (e) => {
+        e.stopPropagation();
+        const target = e.target as HTMLImageElement;
+        if (target.src.includes("images/gallery_slider/img-icon-white.png")) {
+            target.src = "images/gallery_slider/img-icon-pink.png";
+        } else if (target.src.includes("images/gallery_slider/play-icon-white.png")) {
+            target.src = "images/gallery_slider/play-icon-pink.png";
+        }
+    });
+
+    imgElement.addEventListener("mouseleave", (e) => {
+        e.stopPropagation();
+        const target = e.target as HTMLImageElement;
+        if (target.src.includes("images/gallery_slider/img-icon-pink.png")) {
+            target.src = "images/gallery_slider/img-icon-white.png";
+        } else if (target.src.includes("images/gallery_slider/play-icon-pink.png")) {
+            target.src = "images/gallery_slider/play-icon-white.png";
+        }
+    });
 });
